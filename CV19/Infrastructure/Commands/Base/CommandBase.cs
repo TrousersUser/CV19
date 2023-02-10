e37e59f8336c408d624c059bc5c45b3da419df4e
Iam <me_is_nikita22@mail.ru>
2023-02-10 12:44:36 +0300

    ADDING Создание базового класса для команд.

---
+++ CV19/Infrastructure/Commands/Base/CommandBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CV19.Infrastructure.Commands.Base
+{
+    internal abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        public abstract bool CanExecute(object command);
+        public abstract void Execute(object command);
+    }
+}
