5bb95fa26673ac1afbb6a9defa10c4c59c10c4e9
Iam <me_is_nikita22@mail.ru>
2023-02-12 04:18:46 +0300

    ADDING Добавлена серия из методов, в совокупности, создают возможность прочитки заголовков из ресурса, выделение всех дат, для каждой из стран.

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,15 +1,54 @@
 ﻿
+using System.Globalization;
+
 namespace CV19Console
 {
     class Program
     {
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-        static void Main(string[] args)
+        private static async Task<Stream> GetDataStream()
         {
             var client = new HttpClient();
-            var response = client.GetAsync(data_url).Result;
-            string data_str = response.Content.ReadAsStringAsync().Result;
+            var responce = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await responce.Content.ReadAsStreamAsync();
+        }
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
+
+            while (!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    continue;
+
+                yield return line;
+            }
+        }
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(line => DateTime.Parse(line,CultureInfo.InvariantCulture))
+            .ToArray();
+        static void Main(string[] args)
+        {
+            #region Рубрика - эксперррррименты
+            //var client = new HttpClient();
+            //var response = client.GetAsync(data_url).Result;
+            //string data_str = response.Content.ReadAsStringAsync().Result;
+
+            //foreach (string line in GetDataLines())
+            //    Console.WriteLine(line); 
+            #endregion
+
+            foreach (DateTime date in GetDates())
+            {
+                Console.WriteLine($"\r{date.ToShortDateString()}\n"); ;
+            }
+
             Console.ReadLine();
         }
     }
