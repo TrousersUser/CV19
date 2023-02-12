906bf7e295fabcd41fcb789c3046e2e68d8a160c
Iam <me_is_nikita22@mail.ru>
2023-02-12 03:14:39 +0300

    ADDING Создание запроса на получение данных из выбранного ресурса.

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,5 +1,17 @@
 ﻿
-static void Main(string[] args)
+namespace CV19Console
 {
+    class Program
+    {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync(data_url).Result;
+            string data_str = response.Content.ReadAsStringAsync().Result;
+            Console.ReadLine();
+        }
+    }
 }
+
