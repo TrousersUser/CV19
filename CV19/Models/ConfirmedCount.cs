f79691315b0ff30c24d6edaf0d652a9743d5e7a1
Iam <me_is_nikita22@mail.ru>
2023-02-15 04:25:23 +0300

    ADDING Создание сущностей, участвующих в программе.

---
+++ CV19/Models/ConfirmedCount.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CV19.Models
+{
+    internal struct ConfirmedCount
+    {
+        public DateTime Date { get; set; }
+
+        public int Count { get; set; }
+
+    }
+}
---
+++ CV19/Models/PlaceInfo.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace CV19.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Point - structure, what gives an opportunity to use X,Y points, containing any of numeric, in int type, information
+        /// </summary>
+        public Point Location { get; set; }
+        public IEnumerable<ConfirmedCount> Confirmed { get; set; }
+
+    }
+}
