7a570391cdacf8fe3bdcf1cac8985ace67c11306
Iam <me_is_nikita22@mail.ru>
2023-03-03 18:52:53 +0300

    UPDATE Cleaning process

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -7,9 +7,6 @@ namespace CV19
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();      
-        }
+        public MainWindow() => InitializeComponent();
     }
 }
