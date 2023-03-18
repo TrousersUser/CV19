8cb7331d42c95f7af90e46156da9f3114f18ef36
Iam <me_is_nikita22@mail.ru>
2023-03-18 12:52:58 +0300

    ADD Добавлена сортировка данных; Возможность сгруппировать данные, выводимые через визуальный элемент DataGrid, в представлении главного окна,с применением CollectionViewSource.

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -9,27 +9,27 @@ namespace CV19
     {
         public MainWindow() => InitializeComponent();
 
-        private void CollectionViewSource_Filter(object sender, System.Windows.Data.FilterEventArgs e)
-        {
-            if (!(e.Item is Models.Decanat.Group group)) return;
-            else if (group.Name is null) return;
+        //private void CollectionViewSource_Filter(object sender, System.Windows.Data.FilterEventArgs e)
+        //{
+        //    if (!(e.Item is Models.Decanat.Group group)) return;
+        //    else if (group.Name is null) return;
         
 
-            string filterText = GroupNameFilterText.Text;
-            if (filterText.Length == 0) return;
+        //    string filterText = GroupNameFilterText.Text;
+        //    if (filterText.Length == 0) return;
 
-            if (group.Name.Contains(filterText, System.StringComparison.OrdinalIgnoreCase)) return;
-            else if (group.Description != null &&
-                group.Description.Contains(filterText, System.StringComparison.OrdinalIgnoreCase)) return;
+        //    if (group.Name.Contains(filterText, System.StringComparison.OrdinalIgnoreCase)) return;
+        //    else if (group.Description != null &&
+        //        group.Description.Contains(filterText, System.StringComparison.OrdinalIgnoreCase)) return;
 
-            e.Accepted = false;
-        }
+        //    e.Accepted = false;
+        //}
 
-        private void GroupNameFilterText_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-        {
-            var textBox = (System.Windows.Controls.TextBox)sender;
-            var collection = (System.Windows.Data.CollectionViewSource)textBox.FindResource("GroupsCollection");
-            collection.View.Refresh();
-        }
+        //private void GroupNameFilterText_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        //{
+        //    var textBox = (System.Windows.Controls.TextBox)sender;
+        //    var collection = (System.Windows.Data.CollectionViewSource)textBox.FindResource("GroupsCollection");
+        //    collection.View.Refresh();
+        //}
     }
 }
