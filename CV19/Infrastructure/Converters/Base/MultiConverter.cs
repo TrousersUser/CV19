c26f707bd747033aabe5e3e89aa3f96ff65c850e
FirstS3ason <onionmarker@gmail.com>
2023-04-08 11:32:36 +0300

    Применение класса реализующего IMultiValueConverter интерфейс, для обработки получаемой информации, помещаемой в массив, выводимый в зависимом представлении.

---
+++ CV19/Infrastructure/Converters/Base/MultiConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters.Base
+{
+    internal abstract class MultiConverter : IMultiValueConverter
+    {
+        public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+        public virtual object[] ConvertBack(object value, Type[] types, object parameter, CultureInfo culture)
+            => throw new NotSupportedException($"Обратное преобразование значения {nameof(value)} не поддерживается.");
+
+    }
+}
